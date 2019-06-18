87b4be4597f7f7b38041cceb45c8aae1e0d73e57
Stefan Scepanovic <scepa91@icloud.com>
2019-06-18 10:13:40 -0500

    done

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -15,9 +15,12 @@ class AvailabilitiesController < ApplicationController
     def new
         @start_times = ["1:00 PM", "1:30 PM", "2:00 PM", "2:30 PM", "3:00 PM", "3:30 PM", "4:00 PM", "4:30 PM", "5:00 PM", "5:30 PM", "6:00 PM", "6:30 PM", "7:00 PM", "7:30 PM", "8:00 PM", "8:30 PM"]
         @end_times = ["1:30 PM", "2:00 PM", "2:30 PM", "3:00 PM", "3:30 PM", "4:00 PM", "4:30 PM", "5:00 PM", "5:30 PM", "6:00 PM", "6:30 PM", "7:00 PM", "7:30 PM", "8:00 PM", "8:30 PM", "9:00 PM"]
+        @DAYS = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+        # validates_inclusion_of :day, :in => @DAYS
     end
 
     def create
+        
         start_times = ["1:00 PM", "1:30 PM", "2:00 PM", "2:30 PM", "3:00 PM", "3:30 PM", "4:00 PM", "4:30 PM", "5:00 PM", "5:30 PM", "6:00 PM", "6:30 PM", "7:00 PM", "7:30 PM", "8:00 PM", "8:30 PM"]
         end_times = ["1:30 PM", "2:00 PM", "2:30 PM", "3:00 PM", "3:30 PM", "4:00 PM", "4:30 PM", "5:00 PM", "5:30 PM", "6:00 PM", "6:30 PM", "7:00 PM", "7:30 PM", "8:00 PM", "8:30 PM", "9:00 PM"]
         end_time = end_times[start_times.index(allowed_params[:start_time])]
